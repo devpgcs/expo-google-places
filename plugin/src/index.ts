f44de2c4eca169c72bbce9d2a9e6ba51bc5039d6
Pedro Castro <devpgcs@hotmail.com>
2023-06-23 11:55:56 -0500

    [Plugin] Tests config/internal documentation

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -1,5 +1,7 @@
 import { ConfigPlugin, withInfoPlist } from "@expo/config-plugins";
 
+// This is not being tested since is not a complex plugin and it's not worth it. Also, the best practices don't apply here.
+// More info in: https://docs.expo.dev/config-plugins/development-and-debugging/#plugin-development-best-practices
 const withGoogleApiKey: ConfigPlugin<string> = (config, googlePlacesAPIKey) =>
   withInfoPlist(config, (finalConfig) => {
     finalConfig.modResults.GMSPlacesAPIKey = googlePlacesAPIKey;
